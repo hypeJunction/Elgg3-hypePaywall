10a991dd1681af7b7839f8d322b5fdd6f6209bf8
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2018-05-11 13:54:31 +0200

    fix(fields): use field class

--- classes/hypeJunction/Paywall/AddFormField.php
+++ classes/hypeJunction/Paywall/AddFormField.php
@@ -2,8 +2,6 @@
 
 namespace hypeJunction\Paywall;
 
-use hypeJunction\Payments\Amount;
-
 class AddFormField {
 
 	/**
@@ -17,103 +15,15 @@ class AddFormField {
 
 		$fields = $hook->getValue();
 
-		$fields['paid_access'] = [
-			'#type' => 'paywall/access',
-			'#getter' => function (\ElggEntity $entity) {
-				if (!$entity->paid_access_plans && !$entity->paid_access_amount && !$entity->paid_access_currency) {
-					return null;
-				}
+		$fields['paid_access'] = new PaidAccessField([
+			'type' => 'paywall/access',
+			'is_profile_field' => false,
+		]);
 
-				return [
-					'plans' => $entity->paid_access_plans,
-					'price' => new Amount((int) $entity->paid_access_amount, $entity->paid_access_currency),
-				];
-			},
-			'#setter' => function (\ElggEntity $entity, $value) {
-				$plans = elgg_extract('plans', $value);
-				$price = elgg_extract('price', $value);
-				$amount = elgg_extract('amount', $price, '0');
-				$currency = elgg_extract('currency', $price);
-
-				$amount = Amount::fromString($amount, $currency);
-
-				$entity->paid_access_plans = $plans;
-				$entity->paid_access_amount = $amount->getAmount();
-				$entity->paid_access_currency = $amount->getCurrency();
-			},
-			'#visibility' => function (\ElggEntity $entity) use ($hook) {
-				$can_sell = elgg_is_admin_logged_in();
-
-				$params = ['entity' => $entity];
-				$can_sell = elgg_trigger_plugin_hook('permissions_check:sell', 'object', $params, $can_sell);
-
-				if (!$can_sell) {
-					return false;
-				}
-
-				$params = [
-					'entity' => $entity,
-				];
-
-				return $hook->elgg()->hooks->trigger(
-					'uses:paid_access',
-					"$entity->type:$entity->subtype",
-					$params,
-					true
-				);
-			},
-			'#section' => 'sidebar',
-			'#profile' => false,
-		];
-
-		$fields['paid_download'] = [
-			'#type' => 'paywall/download',
-			'#getter' => function (\ElggEntity $entity) {
-				if (!$entity->paid_download_plans && !$entity->paid_download_amount && !$entity->paid_download_currency) {
-					return null;
-				}
-
-				return [
-					'plans' => $entity->paid_download_plans,
-					'price' => new Amount((int) $entity->paid_download_amount, $entity->paid_download_currency),
-				];
-			},
-			'#setter' => function (\ElggEntity $entity, $value) {
-				$plans = elgg_extract('plans', $value);
-				$price = elgg_extract('price', $value);
-				$amount = elgg_extract('amount', $price, '0');
-				$currency = elgg_extract('currency', $price);
-
-				$amount = Amount::fromString($amount, $currency);
-
-				$entity->paid_download_plans = $plans;
-				$entity->paid_download_amount = $amount->getAmount();
-				$entity->paid_download_currency = $amount->getCurrency();
-			},
-			'#visibility' => function (\ElggEntity $entity) use ($hook) {
-				$can_sell = elgg_is_admin_logged_in();
-
-				$params = ['entity' => $entity];
-				$can_sell = elgg_trigger_plugin_hook('permissions_check:sell', 'object', $params, $can_sell);
-
-				if (!$can_sell) {
-					return false;
-				}
-
-				$params = [
-					'entity' => $entity,
-				];
-
-				return $hook->elgg()->hooks->trigger(
-					'uses:paid_download',
-					"$entity->type:$entity->subtype",
-					$params,
-					true
-				);
-			},
-			'#section' => 'sidebar',
-			'#profile' => false,
-		];
+		$fields['paid_download'] = new PaidDownloadField([
+			'type' => 'paywall/download',
+			'is_profile_field' => false,
+		]);
 
 		return $fields;
 	}
---
+++ classes/hypeJunction/Paywall/PaidDownloadField.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace hypeJunction\Paywall;
+
+use ElggEntity;
+use hypeJunction\Fields\Field;
+use hypeJunction\Payments\Amount;
+use Symfony\Component\HttpFoundation\ParameterBag;
+
+class PaidDownloadField extends Field {
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function isVisible(ElggEntity $entity, $context = null) {
+		if (!parent::isVisible($entity, $context)) {
+			return false;
+		}
+
+		$can_sell = elgg_is_admin_logged_in();
+
+		$params = ['entity' => $entity];
+		$can_sell = elgg_trigger_plugin_hook('permissions_check:sell', 'object', $params, $can_sell);
+
+		if (!$can_sell) {
+			return false;
+		}
+
+		$params = [
+			'entity' => $entity,
+		];
+
+		return elgg()->hooks->trigger(
+			'uses:paid_download',
+			"$entity->type:$entity->subtype",
+			$params,
+			true
+		);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function save(ElggEntity $entity, ParameterBag $parameters) {
+		$value = $parameters->get($this->name);
+
+		$plans = elgg_extract('plans', $value);
+		$price = elgg_extract('price', $value);
+		$amount = elgg_extract('amount', $price, '0');
+		$currency = elgg_extract('currency', $price);
+
+		$amount = Amount::fromString($amount, $currency);
+
+		$entity->paid_download_plans = $plans;
+		$entity->paid_download_amount = $amount->getAmount();
+		$entity->paid_download_currency = $amount->getCurrency();
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function retrieve(ElggEntity $entity) {
+		if (!$entity->paid_download_plans && !$entity->paid_download_amount && !$entity->paid_download_currency) {
+			return null;
+		}
+
+		return [
+			'plans' => $entity->paid_download_plans,
+			'price' => new Amount((int) $entity->paid_download_amount, $entity->paid_download_currency),
+		];
+	}
+}
